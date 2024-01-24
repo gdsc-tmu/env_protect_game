38bf3cc1645fb4dba333cc745d8455241ccb2a2e
yuyuslab <yuyuslab@gmail.com>
2024-01-24 21:08:42 +0900

    ゲーム後にページ遷移してゲーム結果に応じてニュースを表示

--- app_env_protect_game/urls.py
+++ app_env_protect_game/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path
 from . import views
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
 app_name = "app_env_protect_game"
 
 urlpatterns = [
         path('', views.index, name="index"),
-        #path('newsAPItest/', views.newsAPItest, name="newsAPItest"),
-        #path('gameResult/', views.gameResult, name="gameResult"),
+        path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('app_env_protect_game/img/favicon.ico'))),
+        path('newsFeed/', views.getNewsFeed, name="getNewsFeed"),
+        path('result/', views.resultRedirect, name="resultRedirect"),
 
 ]
+
+# Favicon setting is referred to https://ordinarycoders.com/blog/article/add-a-custom-favicon-to-your-django-web-app
 
